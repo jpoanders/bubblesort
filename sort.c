633086cdd4f944cdb3f038a976e9981177d5dec6
João Anders <jpoanders@gmail.com>
2025-06-25 18:56:22 -0300

    make new implementation, similar performance

--- sort.c
+++ sort.c
@@ -6,9 +6,8 @@
 #include "thread_pool.h"
 
 typedef struct {
-    int* v;
-    int size;
     int* vector;
+    int size;
     int lower_bound, upper_bound;
 } bubble_sort_args;
 
@@ -32,8 +31,26 @@ void bubble_sort(int *v, int tam){
 
 void bubble_sort_wrapper(void* args) {
     bubble_sort_args* bs_args = (bubble_sort_args*)(args);
-    bubble_sort(bs_args->v, bs_args->size);
+    int* auxiliar_vector = malloc(sizeof(int)*bs_args->size);
+    int subvector_size = 0;
+    int lower_bound = bs_args->lower_bound, upper_bound = bs_args->upper_bound;
+    int* vector = bs_args->vector;
+    for (int i = 0; i <= bs_args->size; i++) {
+        if (lower_bound <= vector[i] && vector[i] <= upper_bound) {
+            auxiliar_vector[subvector_size] = vector[i];
+            subvector_size++;
+        }
+    }
+    int* subvector = malloc(sizeof(int)*subvector_size);
+    for (int i = 0; i < subvector_size; i++) {
+        subvector[i] = auxiliar_vector[i];
+    }
+    free(auxiliar_vector);
+    bs_args->size = subvector_size;
+    bs_args->vector = subvector;
+    bubble_sort(subvector, subvector_size);
 }
+
 
 // Funcao para imprimir um vetor. Não pode alterar.
 void imprime_vet(unsigned int *v, int tam) {
@@ -68,72 +85,36 @@ int le_vet(char *nome_arquivo, unsigned int *v, int tam) {
 int sort_paralelo(unsigned int *vetor, unsigned int tam, unsigned int ntasks, unsigned int nthreads) {
     int range = tam / ntasks; // Quantidade de números representáveis em cada vetor da task
     int remainder = tam % ntasks;
-    
-    int* task_vector_size = malloc(sizeof(int)*ntasks); // Armazena de facto a quantidade de números em cada vetor da task
-    int** task_vector = malloc(sizeof(int*)*ntasks);  // Array para vetores com tamanho variável
-
     // variáveis do loop
     int lower_bound = 0;
     int upper_bound = range - 1;
-    int* auxiliar_vector = malloc(sizeof(int)*tam);
 
-    for (int j = 0; j < ntasks; j++) {
-        if (remainder) {
-            upper_bound++;
-            remainder--;
-        }
-        task_vector_size[j] = 0;
-        int element_index = 0;
-        for (int i = 0; i < tam; i++) {
-            if ((vetor[i] <= upper_bound) && (vetor[i] >= lower_bound)) {
-                task_vector_size[j]++;
-                auxiliar_vector[element_index] = vetor[i];
-                element_index++;
-            }
-        } 
-        // alocação do subvetor
-        if (task_vector_size[j] == 0) task_vector[j] = NULL;
-        else task_vector[j] = malloc(sizeof(int)*task_vector_size[j]);
-        for (int i = 0; i < task_vector_size[j]; i++) {
-            task_vector[j][i] = auxiliar_vector[i];
-        }
-        lower_bound = upper_bound + 1;
-        upper_bound = upper_bound + range;
-    }  
-    
-    free(auxiliar_vector);
-    for (int i = 0; i < ntasks; i++) {
-        if (task_vector[i] == NULL) printf("tentando acessar vetor NULL");
-        imprime_vet((unsigned int *)task_vector[i], task_vector_size[i]);
-    }
     bubble_sort_args args[ntasks];
     thread_pool_t pool;
+    
     thread_pool_init(&pool, nthreads, ntasks);
-    // adiciona as tasks à pool
     for (int i = 0; i < ntasks; i++) {
-        args[i].v = task_vector[i];
-        args[i].size = task_vector_size[i];
+        upper_bound += (i < remainder) ? 1:0;
+        args[i].vector = (int*) vetor;
+        args[i].size = (int) tam;
+        args[i].lower_bound = lower_bound;
+        args[i].upper_bound = upper_bound;
         thread_pool_add_task(&pool, bubble_sort_wrapper, &args[i]);
+        lower_bound = upper_bound + 1;
+        upper_bound = upper_bound + range;
     }
     thread_pool_destroy(&pool);
-    printf("destrui a pool.\n");
-    fflush(stdout);
-
-    for (int i = 0; i < ntasks; i++) {
-        imprime_vet((unsigned int *)task_vector[i], task_vector_size[i]);
-    }
     
         // concatena subvetores
     int pos = 0;
     for (int i = 0; i < ntasks; i++) {
-        for (int j = 0; j < task_vector_size[i]; j++)
-            vetor[pos++] = task_vector[i][j];
+        for (int j = 0; j < args[i].size; j++)
+            vetor[pos++] = args[i].vector[j];
     }
     
-    free(task_vector_size);
-    for (int i = 0; i < ntasks; i++)
-        free(task_vector[i]);
-    free(task_vector);
+    for (int i = 0; i < ntasks; i++) {
+        free(args[i].vector);
+    }
 
     return 1;
 }
